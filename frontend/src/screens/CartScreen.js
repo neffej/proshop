07582cf9d4ba2c4037ff377ba31fc3f072ffddf3
neffej <118024182+neffej@users.noreply.github.com>
2023-04-27 17:37:11 -0400

    added cart functionality to cartScreens

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useParams, useSearchParams, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
 
 const CartScreen = () => {
+  let { id } = useParams()
+
+  const [searchParams] = useSearchParams()
+  let qty = searchParams.get('qty')
+
+  const dispatch = useDispatch()
+
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart
+
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id, qty))
+    }
+  }, [dispatch, id, qty])
+
   return <div>Cart</div>
 }
 
